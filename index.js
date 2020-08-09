3cf1e9d5bc749bd6fb702ec4cc37db04d2d03a69
awaji-mitop GILBERT <successgilli@gmail.com>
2020-08-09 12:00:59 +0100

    added docker to node-mongo app

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Connect to MongoDB
 mongoose
   .connect(
-    'mongodb://mongo:27017/docker-node-mongo',
+    'mongodb://mongo:27017/docker-mongo-and-node',
     { useNewUrlParser: true }
   )
   .then(() => console.log('MongoDB Connected'))
